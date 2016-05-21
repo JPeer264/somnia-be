19e2cd065ba56788fd385d563b73cf696b96620d
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-05-21 11:23:12 +0200

    Adds Project.update

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -35,11 +35,33 @@ module.exports = {
 
   update: function (req,res) {
 
-    var id = param('id');
-    
-    
-    
-    
+    var projectId = req.param('id'),
+        userId    = req.token.id,
+        body      = req.body;
+
+    if(body.dueDate){
+      body.dueDate = sails.moment(parseInt(body.dueDate)).format('YYYY-MM-DD');
+    }
+
+    Project.checkOwnership(userId, projectId, function(err, isOwner){
+      if(err) return res.negotiate(err);
+
+      if(isOwner){
+        Project.update({id: projectId}, body)
+          .then(function(project){
+            return res.json(200, {project: project});
+          })
+          .fail(function(err){
+            return res.negotiate(err);
+          });
+      }else{
+        return res.json(401, {err:'You are not authorized to update this project'});
+      }
+
+
+    });
+
+
   },
 
   delete: function (req,res) {
